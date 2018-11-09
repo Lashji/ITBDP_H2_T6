3a2cee19bee2f1dfbb2a8e44248b3c8e4a7323f2
lashj <lassi.palojarvi@outlook.com>
2018-11-09 14:47:15 +0200

    drafting

---
+++ linespeeds.py
@@ -0,0 +1,33 @@
+from PIL import Image, ImageDraw, ImageFont
+
+
+def main():
+    img = openImage("tampere.png")
+
+
+def openImage(img_name):
+    return Image.open(img_name)
+
+
+def read_data(data_file):
+    ""
+
+
+def deg2num():
+    ""
+
+
+def draw_busses():
+    ""
+
+
+def draw_one_buss(buss):
+    ""
+
+
+def draw():
+    ""
+
+
+def draw_not_overlapping():
+    ""
